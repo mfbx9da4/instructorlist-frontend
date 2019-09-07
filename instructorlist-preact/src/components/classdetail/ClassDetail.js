cf33574681b40bcec3a08a162200fbd233c2709c
David Adler <dalberto.adler@gmail.com>
2019-09-07 03:47:54 +0100

    fix payment state shown first

--- instructorlist-preact/src/components/classdetail/ClassDetail.js
+++ instructorlist-preact/src/components/classdetail/ClassDetail.js
@@ -10,7 +10,7 @@ export default class ClassDetail extends Component {
     super(props)
     this.state = {
       item: this.props.data.state.classes[props.matches.id],
-      showPayment: true,
+      showPayment: false,
     }
   }
 
