50c465168b4dd2d573a83d23a3e7861e6bc2ee6f
David Adler <dalberto.adler@gmail.com>
2019-09-14 13:28:29 +0100

    Copied across frontend

---
+++ instructorlist-express/frontend-build-copy/build/precache-manifest.370d2196e44c61deddda70f2b02e30b5.js
@@ -0,0 +1,231 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/0f00c5c21e063e58eb7fbcbb3a143cfa.svg"
+  },
+  {
+    "url": "/7e967092c5741e412e6fa4013668db1d.svg"
+  },
+  {
+    "url": "/850e96784fde44d921f162f048f7f367.svg"
+  },
+  {
+    "revision": "8173a43ad0f009abae87a7516006c1fb",
+    "url": "/assets/.DS_Store"
+  },
+  {
+    "revision": "53ac170e970ad034a55ee15ce198708c",
+    "url": "/assets/favicon.ico"
+  },
+  {
+    "revision": "5cee1e4c6643f42c2c617be141d6580a",
+    "url": "/assets/fonts/.DS_Store"
+  },
+  {
+    "revision": "5c55c9c311ea4182d284de4f249beaf9",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Black Italic.otf"
+  },
+  {
+    "revision": "f401366193520cdf512c7ade98260e27",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Black.otf"
+  },
+  {
+    "revision": "2f043dc9e72e921e5e44413625acce20",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Bold Italic.otf"
+  },
+  {
+    "revision": "62d4d7d369292a9bf23762465ec6d704",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Bold.otf"
+  },
+  {
+    "revision": "c69a08020a247925b81e984b9a44b511",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Extrabold Italic.otf"
+  },
+  {
+    "revision": "b4f9eb8ce027016ab9b9860817451d07",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Extrabold.otf"
+  },
+  {
+    "revision": "748f66fdaa1cb98a4751ce334d8f9e9a",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Light Italic.otf"
+  },
+  {
+    "revision": "076d851b602b9915c429f3a2e436c639",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Light.otf"
+  },
+  {
+    "revision": "96b34dd3fa3bf9942d57bb3b67a6f0ba",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Regular Italic.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Regular.otf"
+  },
+  {
+    "revision": "68f6e78569a41b68fdcfbb7185e896bf",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Semibold Italic.otf"
+  },
+  {
+    "revision": "6a386899746222073dd64c5f74d1a69d",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Semibold.otf"
+  },
+  {
+    "revision": "2b4b5e223982749696b82b76d6e14066",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Thin Italic.otf"
+  },
+  {
+    "revision": "8f0bc01ce5e5becef482d277cb72b728",
+    "url": "/assets/fonts/Proxima Nova/Mark Simonson - Proxima Nova Thin.otf"
+  },
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "e4144d1edec6e71de202159b05e418a3",
+    "url": "/assets/images/chevron.svg"
+  },
+  {
+    "revision": "da30988da8ad61b960a91a91b02d28e8",
+    "url": "/assets/images/class.jpeg"
+  },
+  {
+    "revision": "850e96784fde44d921f162f048f7f367",
+    "url": "/assets/images/cow.svg"
+  },
+  {
+    "revision": "89204b62d1fa66bf9f9ab5b383ab67b6",
+    "url": "/assets/images/dancing.gif"
+  },
+  {
+    "revision": "0f00c5c21e063e58eb7fbcbb3a143cfa",
+    "url": "/assets/images/directions.svg"
+  },
+  {
+    "revision": "dd7486f48d68817946deff5725c828c9",
+    "url": "/assets/images/filters.svg"
+  },
+  {
+    "revision": "9c94f13b27f50718300766d6aac8b326",
+    "url": "/assets/images/logo.svg"
+  },
+  {
+    "revision": "bf1f1b0fe4ee472995641d2c06ded0d4",
+    "url": "/assets/images/map.svg"
+  },
+  {
+    "revision": "7e967092c5741e412e6fa4013668db1d",
+    "url": "/assets/images/shrug.svg"
+  },
+  {
+    "revision": "e167be5b35cc2180949fba6cdd4e65e2",
+    "url": "/assets/images/tick-blue.svg"
+  },
+  {
+    "revision": "eda3aa5f5e28c87cdc3b278fe3b843e4",
+    "url": "/assets/images/tick.svg"
+  },
+  {
+    "url": "/bf1f1b0fe4ee472995641d2c06ded0d4.svg"
+  },
+  {
+    "revision": "beba3b423abe512ca553",
+    "url": "/bundle.beba3.js"
+  },
+  {
+    "revision": "beba3b423abe512ca553",
+    "url": "/bundle.beba3.js.map"
+  },
+  {
+    "url": "/dd7486f48d68817946deff5725c828c9.svg"
+  },
+  {
+    "url": "/e167be5b35cc2180949fba6cdd4e65e2.svg"
+  },
+  {
+    "url": "/e4144d1edec6e71de202159b05e418a3.svg"
+  },
+  {
+    "revision": "53ac170e970ad034a55ee15ce198708c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "654c1eb9da3ff9630f19f7af029f3ffe",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "9aa7b7886544390ddcdc",
+    "url": "/polyfills.9aa7b.js"
+  },
+  {
+    "revision": "9aa7b7886544390ddcdc",
+    "url": "/polyfills.9aa7b.js.map"
+  },
+  {
+    "revision": "d05333d2e4ddf82fd36644e518329a28",
+    "url": "/push-manifest.json"
+  },
+  {
+    "revision": "f66f3968270852e155e0",
+    "url": "/route-class.page.chunk.f66f3.js"
+  },
+  {
+    "revision": "f66f3968270852e155e0",
+    "url": "/route-class.page.chunk.f66f3.js.map"
+  },
+  {
+    "revision": "3cb744592383db88e505",
+    "url": "/route-home.chunk.3cb74.js"
+  },
+  {
+    "revision": "3cb744592383db88e505",
+    "url": "/route-home.chunk.3cb74.js.map"
+  },
+  {
+    "revision": "42d0f792e54260ddf85a",
+    "url": "/route-profile.chunk.42d0f.js"
+  },
+  {
+    "revision": "42d0f792e54260ddf85a",
+    "url": "/route-profile.chunk.42d0f.js.map"
+  },
+  {
+    "revision": "60505b3a60c4710dfc90",
+    "url": "/route-search.chunk.60505.js"
+  },
+  {
+    "revision": "60505b3a60c4710dfc90",
+    "url": "/route-search.chunk.60505.js.map"
+  },
+  {
+    "revision": "ea16600d59ac912ec0e67062959df5d5",
+    "url": "/shell/index.html"
+  },
+  {
+    "revision": "beba3b423abe512ca553",
+    "url": "/style.72067.css"
+  },
+  {
+    "revision": "beba3b423abe512ca553",
+    "url": "/style.72067.css.map"
+  }
+]);
