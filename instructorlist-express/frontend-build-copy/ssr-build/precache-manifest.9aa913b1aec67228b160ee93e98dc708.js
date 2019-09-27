08d6cc5e9e25a0b292448f8cd707f9cdabc3e9e7
David Adler <dalberto.adler@gmail.com>
2019-09-27 18:25:58 +0100

    Copied across frontend

---
+++ instructorlist-express/frontend-build-copy/ssr-build/precache-manifest.9aa913b1aec67228b160ee93e98dc708.js
@@ -0,0 +1,45 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/073519f75df9d797b307d8e3a13bb060.svg"
+  },
+  {
+    "url": "/0f00c5c21e063e58eb7fbcbb3a143cfa.svg"
+  },
+  {
+    "url": "/7e967092c5741e412e6fa4013668db1d.svg"
+  },
+  {
+    "url": "/850e96784fde44d921f162f048f7f367.svg"
+  },
+  {
+    "url": "/bf1f1b0fe4ee472995641d2c06ded0d4.svg"
+  },
+  {
+    "url": "/dd7486f48d68817946deff5725c828c9.svg"
+  },
+  {
+    "url": "/e167be5b35cc2180949fba6cdd4e65e2.svg"
+  },
+  {
+    "url": "/e4144d1edec6e71de202159b05e418a3.svg"
+  },
+  {
+    "url": "/eda3aa5f5e28c87cdc3b278fe3b843e4.svg"
+  },
+  {
+    "revision": "ffe646080baeca5e49dc",
+    "url": "/ssr-bundle.js"
+  },
+  {
+    "revision": "ffe646080baeca5e49dc",
+    "url": "/ssr-bundle.js.map"
+  },
+  {
+    "revision": "ffe646080baeca5e49dc",
+    "url": "/style.f7db0.css"
+  },
+  {
+    "revision": "ffe646080baeca5e49dc",
+    "url": "/style.f7db0.css.map"
+  }
+]);
