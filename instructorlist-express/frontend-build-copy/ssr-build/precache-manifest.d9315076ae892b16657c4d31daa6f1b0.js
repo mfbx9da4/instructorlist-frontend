4af9e6e8f6b26f8fbed50e04186a2bd355a083ef
David Adler <dalberto.adler@gmail.com>
2019-10-11 17:46:37 +0100

    added ssr build

---
+++ instructorlist-express/frontend-build-copy/ssr-build/precache-manifest.d9315076ae892b16657c4d31daa6f1b0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/044b7d8a4b9f46cdcd9efec7cbf0ce3b.png"
+  },
+  {
+    "url": "/073519f75df9d797b307d8e3a13bb060.svg"
+  },
+  {
+    "url": "/0f00c5c21e063e58eb7fbcbb3a143cfa.svg"
+  },
+  {
+    "url": "/384154096739132becf9e203e7c95bdd.png"
+  },
+  {
+    "url": "/6fb06926b511453cc51459efd8b8342a.png"
+  },
+  {
+    "url": "/74ba314e88cf1ea8c585b1d36e042287.svg"
+  },
+  {
+    "url": "/7e967092c5741e412e6fa4013668db1d.svg"
+  },
+  {
+    "url": "/84946d1d6009e193b72896a56ed10044.png"
+  },
+  {
+    "url": "/850e96784fde44d921f162f048f7f367.svg"
+  },
+  {
+    "url": "/9be0c68fb5c6926bbb14310d97411ba1.png"
+  },
+  {
+    "url": "/9d609a9735179783ce1f896c4e8dc493.png"
+  },
+  {
+    "url": "/9e4607ba1f07e89136e790c60671e5d9.svg"
+  },
+  {
+    "url": "/b98288aa067682318cf3e4fecb6a834d.png"
+  },
+  {
+    "url": "/bf1f1b0fe4ee472995641d2c06ded0d4.svg"
+  },
+  {
+    "url": "/c5f181ff955afaea332f1ceeb52a5440.png"
+  },
+  {
+    "url": "/dd7486f48d68817946deff5725c828c9.svg"
+  },
+  {
+    "url": "/e167be5b35cc2180949fba6cdd4e65e2.svg"
+  },
+  {
+    "url": "/e4144d1edec6e71de202159b05e418a3.svg"
+  },
+  {
+    "url": "/ecbe6deaaaef40d480cfa8e7952c2b4a.png"
+  },
+  {
+    "url": "/eda3aa5f5e28c87cdc3b278fe3b843e4.svg"
+  },
+  {
+    "revision": "f0be98dafefe125d5a3b",
+    "url": "/ssr-bundle.js"
+  },
+  {
+    "revision": "f0be98dafefe125d5a3b",
+    "url": "/ssr-bundle.js.map"
+  },
+  {
+    "revision": "f0be98dafefe125d5a3b",
+    "url": "/style.03855.css"
+  },
+  {
+    "revision": "f0be98dafefe125d5a3b",
+    "url": "/style.03855.css.map"
+  }
+]);
