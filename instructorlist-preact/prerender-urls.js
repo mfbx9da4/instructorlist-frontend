601cc3c69636c461c0ad4dcfb0dd06b7309316ae
David Adler <dalberto.adler@gmail.com>
2019-10-19 16:59:56 +0100

    trying to fix build

---
+++ instructorlist-preact/prerender-urls.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    url: '/',
+    title: 'Instructorlist',
+  },
+  {
+    url: '/shell',
+    title: 'Instructorlist',
+  },
+  {
+    url: '/search',
+    title: 'Search',
+  },
+  {
+    url: '/profile',
+    title: 'Profile',
+  },
+]
